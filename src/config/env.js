b4442f177f4907da8e87fa0a28802ae2669c5f29
Maike <maikonalexandre574@gmail.com>
2024-12-18 15:06:46 -0300

    feat: add api default url

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
   MODE: z.string().default("dev"),
-  VITE_API_URL: z.string(),
+  VITE_API_URL: z.string().default("http://localhost:4000"),
 });
 
 export const env = envSchema.parse(import.meta.env);
